fda4dd3487d441b63b9ccce9b5403b7adf64b9fc
Trevor Carmack <tscarmack77@gmail.com>
2024-01-16 19:22:33 -0600

    added classes 27 and 28

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,14 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
+localStorage.setItem('botScore', 1)
 
+
+
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', addAnotherOne)
+
+function addAnotherOne(){
+    let botScoreVal = Number(localStorage.getItem('botScore'))
+    botScoreVal +=1
+    localStorage.setItem('botScore', botScoreVal)
+}
--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,21 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+class NetflixTVShow{
+    constructor(showName,showCast,showBudget,runTime){
+        this.name=showName
+        this.cast=showCast
+        this.showBudget=showBudget
+        this.runTime=runTime
+    }
+    jumpShark(){
+        alert('everyone DIES')
+    }
+    commercialBreak(){
+        alert('Don\'t go anywhere!')
+    }
+    newShowAlert(){
+        alert('coming this fall!')
+    }
+}
 
 
+let strangerThings = new NetflixTVShow("Stranger Things", "Finn Wolfhard", "$86 Million", "1 hour")
