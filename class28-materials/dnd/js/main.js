d14aa512c30191292da2973848014362708460a9
Trevor Carmack <tscarmack77@gmail.com>
2024-01-21 18:43:40 -0600

    fixed class 16 normalize.css error

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -1,15 +1,26 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
+    document.querySelector('ul').innerHTML = ''
   const choice = document.querySelector('input').value
   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data.subclasses)
+        data.subclasses.forEach( obj => {
+            // create li
+            const li = document.createElement('li')
+            // add text to li
+            li.textContent = obj.name
+            // append to dom
+            document.querySelector('ul').appendChild(li)
+    })
       })
+     
       .catch(err => {
           console.log(`error ${err}`)
       });
