90dc17834f353cd6a22110c44aac1ec58e176c1c
Trevor Carmack <tscarmack77@gmail.com>
2024-02-12 17:33:19 -0600

    updated super review

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -1,31 +1,33 @@
 //Create a conditonal that checks their age
+document.querySelector('h1').addEventListener('click', checkAge)
+let p = document.querySelector('p')
 
-let age = document.querySelector()
 
 //If under 16, tell them they can not drive
-
+function checkAge() {
+    let age = document.querySelector('input').value
 if (age < 16) {
-    return "You can not drive"
+    p.innerText = "You can not drive"
 }
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
 else if (age < 18) {
-    return "You can't hate from outside the club, because you can't even get in"
+    p.innerText = "You can't hate from outside the club, because you can't even get in"
 }
 //If under 21, tell them they can not drink
 else if (age < 21) {
-    return "You cannot drink"
+    p.innerText = "You cannot drink"
 }
 //If under 25, tell them they can not rent cars affordably
 else if (age < 25) {
-    return "Youo can not rent cars affordably"
+    p.innerText = "You can not rent cars affordably"
 }
 //If under 30, tell them they can not rent fancy cars affordably
 else if (age < 30) {
-    return "you can not rent fancy cars affordably"
+    p.innerText = "you can not rent fancy cars affordably"
 }
 //If over 30, tell them there is nothing left to look forward too
 else {
-    return "there is nother left to look forward to"
+    p.innerText = "there is nother left to look forward to"
 }
 
 
@@ -33,4 +35,9 @@ else {
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+
+
+}
+
 
--- super-review/simple-functions/js/main.js
+++ super-review/simple-functions/js/main.js
@@ -1,12 +1,38 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
+function subTwoNums(x, y) {
+    alert(x - y)
+}
 
 //create a function that divides three numbers and console logs the quotient
 
+function divThreeNums(x, y, z) {
+    console.log(x/y/z)
+}
+
 //create a function that multiplys three numbers and returns the product
+function mulThreeNums(x, y, z) {
+    return (x*y*z)
+}
 
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
+function threeNums(x, y, z) {
+    let sum = x + y
+    let remainder = sum % z
+    return remainder
+}
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+
+function fourNums(a, b, c, d) {
+    let product = a * b
+    if (product > 100) {
+        console.log(product + c+d)
+    } else if (product < 100) {
+        console.log(product - c - d)
+    } else {
+        alert((a*b*c)%d)
+    }
+}
